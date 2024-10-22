c59da4a0fbe0af1e19cf44b4b0ba59668b09a733
yeonjoo8231 <77268860+yeonjoo8231@users.noreply.github.com>
2024-10-22 20:50:13 +0900

    init: Next.js 프로젝트 기본 구조 설정

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className='font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20'>
+      <main className='flex flex-col gap-8 row-start-2 items-center sm:items-start'>
+        main
+      </main>
+      <footer className='row-start-3 flex gap-6 flex-wrap items-center justify-center'>
+        footer
+      </footer>
+    </div>
+  );
+}
